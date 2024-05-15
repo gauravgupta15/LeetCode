4219c231ce6a1166ff9076b88a01b1c9c405aad6
Gaurav Gupta <76397561+SpySparker@users.noreply.github.com>
2024-05-15 11:55:08 +0530

    Time: 0 ms (100.00%) | Memory: 10.9 MB (71.95%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        int maxEle = INT_MIN;
+         for(int i=0; i<candies.size(); i++) {
+            maxEle = max(maxEle,candies[i]);
+         }
          vector<bool> ans;
-         int maxEle = *max_element(candies.begin(),candies.end());
          for(int i=0; i<candies.size(); i++) {
             if((candies[i]+extraCandies)>= maxEle) ans.push_back(true);
             else ans.push_back(false);
