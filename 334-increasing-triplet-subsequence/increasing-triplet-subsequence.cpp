0ecc7ee5835c5182d54562c1eb9820bf05cdcfa4
Gaurav Gupta <76397561+SpySparker@users.noreply.github.com>
2024-06-02 18:34:18 +0530

    Time: 82 ms (70.46%) | Memory: 114.3 MB (29.90%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.cpp
@@ -1,34 +1,14 @@
-// class Solution {
-// public:
-    
-//     // bool increasingTriplet(vector<int>& nums) {
-//     //    if(nums.size()<3) return false;
-//     //    int a,b=INT_MAX;
-//     //    for(int num : nums) {
-//     //     if(num <= a) a=num;
-//     //     else if(num <= b) b = num;
-//     //     else return true;
-//     //    } return false;
-//     // }
-    class Solution {
+class Solution {
 public:
-    bool increasingTriplet(std::vector<int>& nums) {
-        if (nums.size() < 3) {
-            return false;
-        }
-        int a = INT_MAX;
-        int b = INT_MAX;
-
-        for (int num : nums) {
-            if (num <= a) {
-                a = num;
-            } else if (num <= b) {
-                b = num;
-            } else {
-                return true;
-            }
-        }
-
-        return false;
+    
+    bool increasingTriplet(vector<int>& nums) {
+       if(nums.size()<3) return false;
+       int a = INT_MAX;
+       int b=INT_MAX;
+       for(int num : nums) {
+        if(num <= a) a=num;
+        else if(num <= b) b = num;
+        else return true;
+       } return false;
     }
 };
