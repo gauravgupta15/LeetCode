0eafb184a512c09b1f4c3a5d239ca8af2e0ca261
Gaurav Gupta <76397561+SpySparker@users.noreply.github.com>
2024-06-13 00:25:15 +0530

    Time: 6 ms (28.53%) | Memory: 11.2 MB (38.21%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -1,17 +1,13 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int,int> mpp;
+        unordered_map<int,int> mpp,check;
         for(auto it : arr) {
             mpp[it]++;
         }
-        vector<int>check;
         for(auto it : mpp) {
-            check.push_back(it.second);
-        }
-        sort(check.begin(),check.end());
-        for(int i=0;i<check.size()-1; i++) {
-            if(check[i] == check[i+1]) return false;
+            check[it.second]++;
+            if(check[it.second]>1) return false;
         }
         return true;
 
