a0900fdefef4cb8c99a3b50223a0852cf07bd1ac
Gaurav Gupta <76397561+gauravgupta15@users.noreply.github.com>
2025-02-02 03:00:23 +0530

    Time: 30 ms (5.08%) | Memory: 58.4 MB (31.94%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -4,8 +4,13 @@ class Solution {
         for(int num : nums) {
             mpp.put(num, mpp.getOrDefault(num, 0)+1);
         }
-        for(Map.Entry<Integer,Integer>entry : mpp.entrySet()) {
-            if(entry.getValue() >= 2) {
+        // for(Map.Entry<Integer,Integer>entry : mpp.entrySet()) {
+        //     if(entry.getValue() >= 2) {
+        //         return true;
+        //     }
+        // }
+        for(int value : mpp.keySet()) {
+            if(mpp.get(value) >= 2) {
                 return true;
             }
         }
