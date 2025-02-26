63c03a14d862674cf9fb0ed31bb989b7f1f12e9d
Gaurav Gupta <76397561+gauravgupta15@users.noreply.github.com>
2025-02-26 08:43:25 +0530

    Time: 104 ms (6.78%) | Memory: 56.3 MB (97.36%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,38 +1,32 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length < 2) return nums.length;
+        if (nums.length < 2)
+            return nums.length;
         int count = 1;
         int maxi = 1;
-        PriorityQueue<Integer>minH = new PriorityQueue<>();
-        for(int num : nums) {
+        PriorityQueue<Integer> minH = new PriorityQueue<>();
+        for (int num : nums) {
             minH.add(num);
         }
 
-
-
-
         int current = minH.peek();
         minH.poll();
-        while(!minH.isEmpty()) {
+        while (!minH.isEmpty()) {
             int next = minH.peek();
-            //--------y
-            if( next == current + 1 ){
-                count ++ ; 
-                //pop the elementt 
-                minH.poll();
-            }else{
-                if( next == current ){
-                    
-                    minH.poll();
+            // --------y
+            if (next == current + 1) {
+                count++;
+                // pop the elementt
+            } else {
+                if (next != current) {
 
-                }else{
-                    count=1;
-                minH.poll();
-                    
+                    count = 1;
+
                 }
             }
+            minH.poll();
             current = next;
-            maxi = Math.max(maxi,count);
+            maxi = Math.max(maxi, count);
         }
         return maxi;
     }
