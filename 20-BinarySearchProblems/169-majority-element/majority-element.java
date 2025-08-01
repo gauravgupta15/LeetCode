2d436a5b695a2f00cb17ef7a5c2aa4a6bcee0b5d
Gaurav Gupta <76397561+gauravgupta15@users.noreply.github.com>
2025-08-01 23:41:46 +0530

    Time: 1 ms (99.86%) | Memory: 53.2 MB (38.00%) - LeetSync

--- 20-BinarySearchProblems/169-majority-element/majority-element.java
+++ 20-BinarySearchProblems/169-majority-element/majority-element.java
@@ -1,22 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int freq = 1;
         int ele = nums[0];
+        int freq  = 1;
         for(int i = 1; i<nums.length; i++) {
-            if(freq == 0) {
+            if(freq  == 0) {
                 ele = nums[i];
-                freq=1;
+                freq++;
             }
-          else if(ele == nums[i]) {
-            freq++;
-          } else {
-            freq--;
-          }
-          
+            else if(ele != nums[i]) {
+                freq--;
+    
+            } else freq++;
         }
+
         return ele;
-
     }
-
-
 }
