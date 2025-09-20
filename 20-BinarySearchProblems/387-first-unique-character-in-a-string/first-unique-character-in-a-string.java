267878551d69e84b161629c62cd37650397df249
Gaurav Gupta <76397561+gauravgupta15@users.noreply.github.com>
2025-09-20 09:00:23 +0530

    Time: 33 ms (23.11%) | Memory: 45.5 MB (19.33%) - LeetSync

---
+++ 20-BinarySearchProblems/387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character,Integer> mpp = new HashMap<>();
+        for(int i = 0; i<s.length(); i++) {
+            mpp.put(s.charAt(i), mpp.getOrDefault(s.charAt(i),0)+1);
+        }
+        for(int i = 0; i<s.length(); i++) {
+            if(mpp.get(s.charAt(i)) == 1) return i;
+        }
+        return -1;
+    }
+}
